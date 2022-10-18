61b00e81072538853dfad8b743bed0a516971d7b
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2022-10-18 18:07:39 +0200

    removed unnecessary space in UserRepository class

--- src/main/java/com/gft/mpo/repository/UserRepository.java
+++ src/main/java/com/gft/mpo/repository/UserRepository.java
@@ -10,5 +10,4 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User,Long> {
     @Query(value = "select u from User u where (LOWER(u.username)=LOWER(:username) or :username is null )")
     List<User> findAll(@Param("username") String username);
-
 }
