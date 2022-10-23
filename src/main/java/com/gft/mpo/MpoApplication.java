d86853b1b69723db4334db4a274ce5076e9d2cfb
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2022-10-23 16:13:19 +0200

    removed unnecessary space in MpoApplication class

--- src/main/java/com/gft/mpo/MpoApplication.java
+++ src/main/java/com/gft/mpo/MpoApplication.java
@@ -2,7 +2,6 @@ package com.gft.mpo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class MpoApplication {
 	public static void main(String[] args) {
