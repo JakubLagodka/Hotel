7ce149020752ac72556ad6c02f4372c160a07ca6
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2022-10-27 11:00:11 +0200

    removed unnecessary spaces in OpenApiConfig class

--- src/main/java/com/gft/mpo/configuration/OpenApiConfig.java
+++ src/main/java/com/gft/mpo/configuration/OpenApiConfig.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
-
 public class OpenApiConfig {
     @Bean
     public OpenAPI openApi(){
