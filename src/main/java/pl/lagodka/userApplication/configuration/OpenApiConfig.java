9406dd4c241545d35aecc1d161e39acbfac420ed
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2022-10-29 09:34:09 +0200

    renamed packages names

--- src/main/java/pl/lagodka/userApplication/configuration/OpenApiConfig.java
+++ src/main/java/pl/lagodka/userApplication/configuration/OpenApiConfig.java
@@ -1,7 +1,6 @@
 package pl.lagodka.userApplication.configuration;
 
 
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
