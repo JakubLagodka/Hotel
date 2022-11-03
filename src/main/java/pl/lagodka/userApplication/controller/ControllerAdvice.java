defe713a9bd3f6c8c0bf7ff1e502cbef7ad3c27e
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2022-11-03 12:36:52 +0100

    removed unnecessary white space

--- src/main/java/pl/lagodka/userApplication/controller/ControllerAdvice.java
+++ src/main/java/pl/lagodka/userApplication/controller/ControllerAdvice.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 @org.springframework.web.bind.annotation.ControllerAdvice
 public class ControllerAdvice {
   @ExceptionHandler(value = UserNotFoundException.class)
