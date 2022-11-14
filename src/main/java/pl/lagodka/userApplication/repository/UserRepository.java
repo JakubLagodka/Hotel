fe082fbfa41bf964dd3a0a9befaae792d42297a5
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2022-11-14 09:56:56 +0100

    removed unnecessary white space

--- src/main/java/pl/lagodka/userApplication/repository/UserRepository.java
+++ src/main/java/pl/lagodka/userApplication/repository/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-
 public interface UserRepository extends JpaRepository<User,Long> {
     @Query(value = "select u from User u where (LOWER(u.username)=LOWER(:username) or :username is null )")
     List<User> findAll(@Param("username") String username);
