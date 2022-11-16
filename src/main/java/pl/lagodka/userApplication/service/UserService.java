87d84b4e5f2fac5cd61fde80d68ff392fc10bd02
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2022-11-16 09:39:02 +0100

    removed unnecessary white space

--- src/main/java/pl/lagodka/userApplication/service/UserService.java
+++ src/main/java/pl/lagodka/userApplication/service/UserService.java
@@ -5,7 +5,6 @@ import pl.lagodka.userApplication.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class UserService {
