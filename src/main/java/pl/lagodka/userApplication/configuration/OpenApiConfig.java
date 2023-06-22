31298176858a9443fd449478fb52477c1d4ead3b
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2023-06-22 09:37:20 +0200

    removed unnecessary name

--- src/main/java/pl/lagodka/userApplication/configuration/OpenApiConfig.java
+++ src/main/java/pl/lagodka/userApplication/configuration/OpenApiConfig.java
@@ -9,7 +9,7 @@ public class OpenApiConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("MPO Api")
-                        .description("This is an example MPO busine")
+                        .description("This is an example MPO busin")
                         .version("")
                         .contact(new Contact()
                                 .email("")));
