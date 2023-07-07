70421defe84f330820d8f542f0e8ff18d87af1b2
jbad <jbad@gft.com>
2023-07-07 09:27:41 +0200

    xc

--- src/main/java/pl/lagodka/userApplication/configuration/OpenApiConfig.java
+++ src/main/java/pl/lagodka/userApplication/configuration/OpenApiConfig.java
@@ -9,7 +9,7 @@ public class OpenApiConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("MPO Api")
-                        .description("This is an exa")
+                        .description("This is an ex")
                         .version("")
                         .contact(new Contact()
                                 .email("")));
