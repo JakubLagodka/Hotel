76457e4c0b06fcc89858a840f5a325d2a2c3193a
JakubLagodka <67454685+JakubLagodka@users.noreply.github.com>
2023-07-24 10:14:22 +0200

    Update OpenApiConfig.java

--- src/main/java/pl/lagodka/userApplication/configuration/OpenApiConfig.java
+++ src/main/java/pl/lagodka/userApplication/configuration/OpenApiConfig.java
@@ -9,7 +9,7 @@ public class OpenApiConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("MPO Api")
-                        .description("This is ")
+                        .description("This is")
                         .version("")
                         .contact(new Contact()
                                 .email("")));
