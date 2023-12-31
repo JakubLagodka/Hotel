dd543f079bb96a67c5cff924dc4073c71d499377
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2023-12-31 09:47:39 +0100

    removed unnecessary name

--- src/main/java/pl/lagodka/userApplication/configuration/OpenApiConfig.java
+++ src/main/java/pl/lagodka/userApplication/configuration/OpenApiConfig.java
@@ -6,6 +6,6 @@ import org.springframework.context.annotation.Bean;
 public class OpenApiConfig {
     @Bean
     public OpenAPI openApi(){
-        return new OpenAP
+        return new OpenA
     }
 }
