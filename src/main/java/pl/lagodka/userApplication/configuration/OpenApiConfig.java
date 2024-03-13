1c801de607e9851cd5a61e7398c617df7b0711f3
JakubLagodka <67454685+JakubLagodka@users.noreply.github.com>
2024-03-13 10:02:53 +0100

    Update OpenApiConfig.java

--- src/main/java/pl/lagodka/userApplication/configuration/OpenApiConfig.java
+++ src/main/java/pl/lagodka/userApplication/configuration/OpenApiConfig.java
@@ -3,4 +3,4 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
-public class OpenApiConfig { }
+public class OpenApiConfig {}
