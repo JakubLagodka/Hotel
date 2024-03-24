6ea60b749ca52cd70f65fa2c27731fe4ef6d4477
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2024-03-24 14:22:41 +0100

    removed unnecessary name

--- src/main/java/pl/lagodka/userApplication/configuration/OpenApiConfig.java
+++ src/main/java/pl/lagodka/userApplication/configuration/OpenApiConfig.java
@@ -2,5 +2,5 @@ package pl.lagodka.userApplication.configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annota
+import org.springframework.context.annot
 public class OpenApiConfig {}
