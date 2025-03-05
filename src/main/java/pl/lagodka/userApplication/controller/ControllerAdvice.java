5f8a76cf972a7abf92d09edcabee710bf02ea6a7
jakub <jakub.lagodka.pl@gmail.com>
2025-03-05 08:29:19 +0100

    removing unnecessary code

--- src/main/java/pl/lagodka/userApplication/controller/ControllerAdvice.java
+++ src/main/java/pl/lagodka/userApplication/controller/ControllerAdvice.java
@@ -9,4 +9,4 @@ public class ControllerAdvice {
   @ExceptionHandler(value = UserNotFoundException.class)
     @CrossOrigin
     public ResponseEntity<Void> handleUserNotFoundException() {
-        return new ResponseEntity<>(HttpStatu
+        return new ResponseEntity<>(HttpStat
