020e040c40484ad7225fc2fe3de746cd39579c5b
jakub <jakub.lagodka.pl@gmail.com>
2025-07-06 21:49:44 +0200

    removing unnecessary code

--- src/main/java/pl/lagodka/userApplication/controller/ControllerAdvice.java
+++ src/main/java/pl/lagodka/userApplication/controller/ControllerAdvice.java
@@ -7,4 +7,3 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @org.springframework.web.bind.annotation.ControllerAdvice
 public class ControllerAdvice {
   @ExceptionHandler(value = UserNotFoundException.class)
-    @
