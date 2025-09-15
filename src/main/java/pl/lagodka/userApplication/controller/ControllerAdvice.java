c905e8b45f5d312f73509a33203355c6efcb9a7c
jakub <jakub.lagodka.pl@gmail.com>
2025-09-15 09:19:41 +0200

    removing unnecessary code

--- src/main/java/pl/lagodka/userApplication/controller/ControllerAdvice.java
+++ src/main/java/pl/lagodka/userApplication/controller/ControllerAdvice.java
@@ -6,4 +6,3 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 @org.springframework.web.bind.annotation.ControllerAdvice
 public class ControllerAdvice {
-  @
