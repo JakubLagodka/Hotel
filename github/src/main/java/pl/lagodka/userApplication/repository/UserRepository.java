4aa243735a82ed1707703f04aaa9f6db67c86da9
jakub <jakub.lagodka.pl@gmail.com>
2026-07-08 09:04:49 +0200

    fix

---
+++ github/src/main/java/pl/lagodka/userApplication/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package pl.lagodka.userApplication.repository;
+
+import pl.lagodka.userApplication.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+public interface UserRepository extends JpaRepository<User,Long> {
+    @Query(value = "select u from User u where (LOWER(u.username)=LOWER(:username) or :username is null )")
+    List<User> findAll(@Param("username") String username);
+}
