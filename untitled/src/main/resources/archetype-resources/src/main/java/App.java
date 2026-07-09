0b3b875810659083ab41c6fb77344a62c0a2a996
jakub <jakub.lagodka.pl@gmail.com>
2026-07-09 11:11:53 +0200

    fix

---
+++ untitled/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $pl.lagodka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
