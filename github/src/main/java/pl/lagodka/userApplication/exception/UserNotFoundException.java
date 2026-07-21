045b516b7c9b6072c1842ebd65a9b5a53b46305c
jakub <jakub.lagodka.pl@gmail.com>
2026-07-21 11:02:24 +0200

    fix

---
+++ github/src/main/java/pl/lagodka/userApplication/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package pl.lagodka.userApplication.exception;
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(){
+        super();
+    }
+    public UserNotFoundException(String message){
+        super(message);
+    }
+}
